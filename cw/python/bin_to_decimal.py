10cd87e8bfe477e33bc50334b3d9ad6cada90ca4
Clint <clintegargo@protonmail.ch>
2021-12-20 18:44:58 +0800

    SOLVED

---
+++ cw/python/bin_to_decimal.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/python
+
+
+def bin_to_decimal(inp):
+    return int(inp, 2)
+
+if __name__ == "__main__":
+    inp = input()
+    print(bin_to_decimal(inp))
