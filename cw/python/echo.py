152ce636c4cd82c968f609ec8eb1b79c5b4d9b58
Clint <clintegargo@protonmail.ch>
2022-01-18 10:31:29 +0800

    SOLVED

---
+++ cw/python/echo.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/5c6dc504abcd1628cd174bea/train/python
+
+
+def echo_program():
+    return open("echo.py", "r").read()
+
+
+if __name__ == "__main__":
+    print(echo_program())
