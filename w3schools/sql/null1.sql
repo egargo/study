c69e056c9d10a3d82bfb2a2f4b239aec179edf6e
Clint <clintegargo@protonmail.ch>
2022-02-21 18:02:29 +0800

    SOLVED

---
+++ w3schools/sql/null1.sql
@@ -0,0 +1,5 @@
+-- Select all records from the Customers where the PostalCode column is empty.
+
+
+SELECT * FROM Customers
+WHERE PostalCode IS NULL;
---
+++ w3schools/sql/null2.sql
@@ -0,0 +1,5 @@
+-- Select all records from the Customers where the PostalCode column is NOT empty.
+
+
+SELECT * FROM Customers
+WHERE PostalCode IS NOT NULL;
