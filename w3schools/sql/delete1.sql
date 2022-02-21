8ffb2b83d20d15b3545078f0052db01439bf6235
Clint <clintegargo@protonmail.ch>
2022-02-21 18:47:19 +0800

    SOLVED

---
+++ w3schools/sql/delete1.sql
@@ -0,0 +1,3 @@
+-- Delete all the records from the Customers table where the Country value is 'Norway'.
+
+DELETE FROM Customers WHERE Country = 'Norway';
---
+++ w3schools/sql/delete2.sql
@@ -0,0 +1,4 @@
+-- Delete all the records from the Customers table.
+
+
+DELETE FROM Customers;
