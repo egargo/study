4ce686d5e85c920e2cd1730c7008753801227e93
Clint <clintegargo@protonmail.ch>
2022-02-21 17:59:17 +0800

    SOLVED

---
+++ w3schools/sql/insert1.sql
@@ -0,0 +1,17 @@
+-- Insert a new record in the Customers table.
+
+
+INSERT INTO Customers(
+    CustomerName,
+    Address,
+    City,
+    PostalCode,
+    Country
+)
+VALUES(
+    'Hekkan Burger',
+    'Gateveien 15',
+    'Sandnes',
+    '4306',
+    'Norway'
+)
