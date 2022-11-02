f60ed1fe071a9233b1aa3d40037dbb596ac31aa5
Clint <clintegargo@protonmail.ch>
2022-11-02 13:05:56 +0800

    SOLVED

---
+++ codewars/python/sentence_smash.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/python
+
+
+def smash(words):
+	return ' '.join(words)
+
+
+if __name__ == '__main__':
+	words = ['hello', 'world', 'this', 'is', 'great']
+	print(smash(words))
