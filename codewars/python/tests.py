71ee55be98a90d5eaef37bc26b7deecc1c9fa429
Clint <clintegargo@protonmail.ch>
2022-11-04 23:34:14 +0800

    del

--- codewars/python/tests.py
+++
@@ -1,5 +0,0 @@
-@test.describe("Only use Attempt Button in this kata")
-def test_group():
-    @test.it("Only use Attempt Button in this kata")
-    def test_case():
-        test.assert_equals(1 + 1, 2)
