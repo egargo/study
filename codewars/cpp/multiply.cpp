5405ab2355ed6355311c9a37d9cc6bc5f06e5284
Clint <clintegargo@protonmail.ch>
2022-11-05 18:15:34 +0800

    UNSOLVED

---
+++ codewars/cpp/multiply.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+    cout << multiply(a, b) << endl;
+
+    return 0;
+}
