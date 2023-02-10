fa9cb314f39d15d5c35542da02ff686fb374c3e3
Clint <clint@pop-os.localdomain>
2023-02-10 11:53:54 +0800

    up

---
+++ uni/itlympics23/amazing_race/solution.py
@@ -0,0 +1,45 @@
+import unittest
+import operator
+
+class VigenereCipher(object):
+	def __init__(self, key, alphabet):
+		self.key = key
+		self.alphabet = alphabet
+		self.alpha = {self.alphabet[i]: i for i in range(len(self.alphabet))}
+		self.length = len(self.alphabet)
+		self.op = {'+': operator.add, '-': operator.sub}
+
+	def check_length(self, text):
+		rem = (len(text) % len(self.key)) if len(text) < len(self.key) else (len(self.key) % len(text))
+		return self.key * rem if rem != 0 else self.key
+
+	def process(self, text, op):
+		if text.isupper(): return text
+		cipher = ''
+		KEY = self.check_length(text)
+		for i in range(len(text)):
+			if self.alpha.get(text[i]) == None: cipher += text[i]
+			if self.alpha.get(KEY[i]) == None: cipher += KEY[i]
+			if self.alpha.get(text[i]) != None and self.alpha.get(KEY[i]) != None:
+				check = (self.op.get(op)(self.alpha.get(text[i]), self.alpha.get(KEY[i])) % self.length)
+				cipher += [i[0] for i in self.alpha.items() if check == i[1]][0]
+		return cipher
+
+	def encode(self, text):
+		return text if text.isupper() else self.process(text, '+')
+
+	def decode(self, text):
+		return text if text.isupper() else self.process(text, '-')
+
+
+
+if __name__ == '__main__':
+	password = 'itlympics'
+	abc = 'abcdefghijklmnopqrstuvwxyz'
+	vc = VigenereCipher(password, abc)
+	# print(vc.encode('it\'s a shift cipher!'))
+	# print(vc.encode('codewars'))
+	text = input()
+	enc = vc.encode(text)
+	print(enc)
+	print(vc.decode(enc))
---
+++ uni/itlympics23/amazing_race/testing.py
@@ -0,0 +1,93 @@
+import unittest
+from solution import VigenereCipher
+
+latin = 'abcdefghijklmnopqrstuvwxyz'
+katakana = 'アイウエオァィゥェォカキクケコサシスセソタチツッテトナニヌネノハヒフヘホマミムメモヤャユュヨョラリルレロワヲンー'
+
+class Test(unittest.TestCase):
+	def alpha_latin_encode(self):
+		encode1 = {
+			'itlympics': 'qmwwyeqek',
+			'USTP': 'USTP',
+			'debugging': 'lxmssvqpy',
+			'ITLMYPICS': 'ITLMYPICS',
+			'python': 'xrefac',
+			'it\'s a shift cipher!': 'qm\'q p uzqye oxxjwz!',
+			'linux': 'tbysj',
+		}
+		encode2 = {
+			'amazingrace': 'ayayqamiaei',
+			'AMAZINGRACE': 'AMAZINGRACE',
+			'vigenere cipher': 'vugdvrxv empteq',
+		}
+
+		vc1 = VigenereCipher('itlympics', latin)
+		print("Successful encoding test lowercase Latin alphabet with key of 'itlympics'")
+		[self.assertEqual(vc1.encode(texts[0]), texts[1]) for texts in encode1.items()]
+
+		vc2 = VigenereCipher('amazingrace', latin)
+		print("Successful encoding test lowercase Latin alphabet with key of 'amazingrace'")
+		[self.assertEqual(vc2.encode(texts[0]), texts[1]) for texts in encode2.items()]
+
+
+	def alpha_latin_decode(self):
+		decode1 = {
+			'qmwwyeqek': 'itlympics',
+			'USTP': 'USTP',
+			'lxmssvqpy': 'debugging',
+			'ITLMYPICS': 'ITLMYPICS',
+			'xrefac': 'python',
+			'qm\'q p uzqye oxxjwz!': 'it\'s a shift cipher!',
+			'tbysj': 'linux',
+		}
+		decode2 = {
+			'ayayqamiaei': 'amazingrace',
+			'AMAZINGRACE': 'AMAZINGRACE',
+			'vugdvrxv empteq': 'vigenere cipher',
+		}
+
+		vc1 = VigenereCipher('itlympics', latin)
+		print("Successful decoding test lowercase Latin alphabet with key of 'itlympics'")
+		[self.assertEqual(vc1.decode(texts[0]), texts[1]) for texts in decode1.items()]
+
+		vc2 = VigenereCipher('amazingrace', latin)
+		print("Successful decoding test lowercase Latin alphabet with key of 'amazingrace'")
+		[self.assertEqual(vc2.decode(texts[0]), texts[1]) for texts in decode2.items()]
+
+			
+
+	def alpha_katakana_encode(self):
+		encode = {
+			'カタカナ': 'タモタワ',
+			'ドモアリガトゴザイマス': 'ドオカセガヨゴザキアニ',
+		}
+
+		vc = VigenereCipher('カタカナ', katakana)
+		print("Successful encoding test Katakana characters with key of 'カタカナ'")
+		[self.assertEqual(vc.encode(texts[0]), texts[1]) for texts in encode.items()]
+
+
+	def alpha_katakana_decode(self):
+		decode = {
+			'タモタワ': 'カタカナ',
+			'ドオカセガヨゴザキアニ':'ドモアリガトゴザイマス',
+		}
+
+		vc = VigenereCipher('カタカナ', katakana)
+		print("Successful decoding test Katakana characters with key of 'カタカナ'")
+		[self.assertEqual(vc.decode(texts[0]), texts[1]) for texts in decode.items()]
+
+
+		
+	def test(self):
+		self.alpha_latin_encode()
+		self.alpha_latin_decode()
+		self.alpha_katakana_encode()
+		self.alpha_katakana_decode()
+
+
+
+
+
+if __name__ == '__main__':
+	unittest.main()
