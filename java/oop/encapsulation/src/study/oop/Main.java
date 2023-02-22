1e1212ce44fe13f338221ddd82400ad6a7bf4d07
Clint <clintegargo@protonmail.ch>
2023-02-22 16:55:37 +0800

    Java OOP Encapsulation

---
+++ java/oop/encapsulation/src/study/oop/Main.java
@@ -0,0 +1,11 @@
+package study.oop;
+
+public class Main {
+	public static void main(String[] args) {
+		Insect insect = new Insect();
+
+		insect.setName("Bee");
+
+		System.out.println(insect.getName());
+	}
+}
