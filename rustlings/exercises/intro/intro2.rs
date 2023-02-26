96db3c0637fd60a5ffeb662955bd89c790c90032
Clint <clintegargo@protonmail.ch>
2023-02-26 13:19:25 +0800

    Rustlings: intro solved

--- rustlings/exercises/intro/intro2.rs
+++ rustlings/exercises/intro/intro2.rs
@@ -1,7 +1,7 @@
 // intro2.rs
 // Make the code print a greeting to the world.
-// Execute `rustlings hint intro2` for a hint.
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-    println!("Hello {}!", "Rust");
+    println!("Hello {}!", "Bee");
 }
