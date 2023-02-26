c97ba75cce4fa9512cca03401a7e6d11013fcc62
Clint <clintegargo@protonmail.ch>
2023-02-26 13:36:06 +0800

    Rustlings: quiz1 solved

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -4,29 +4,31 @@
 // - Functions
 // - If
 
-// Mary is buying apples. One apple usually costs 2 Rustbucks, but if you buy
-// more than 40 at once, each apple only costs 1! Write a function that calculates
-// the price of an order of apples given the quantity bought. No hints this time!
+// Mary is buying apples. The price of an apple is calculated as follows:
+// - An apple costs 2 rustbucks.
+// - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
+// Write a function that calculates the price of an order of apples given
+// the quantity bought. No hints this time!
 
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    if apples > 40 {
+        apples
+    } else {
+        apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
 fn verify_test() {
-    let price1 = calculate_apple_price(35);
-    let price2 = calculate_apple_price(40);
-    let price3 = calculate_apple_price(65);
+    let price1 = calculate_price_of_apples(35);
+    let price2 = calculate_price_of_apples(40);
+    let price3 = calculate_price_of_apples(41);
+    let price4 = calculate_price_of_apples(65);
 
     assert_eq!(70, price1);
     assert_eq!(80, price2);
-    assert_eq!(65, price3);
-}
-
-fn calculate_apple_price(price: i32) -> i32 {
-	if price > 40 {
-		return price;
-	}
-
-	price * 2
+    assert_eq!(41, price3);
+    assert_eq!(65, price4);
 }
