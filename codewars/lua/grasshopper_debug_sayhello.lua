a5ce2ac494fe72004a655a0ff6247fcf614c0dd7
Clint Egargo <clintegargo@protonmail.ch>
2023-06-15 12:21:19 +0800

    Codewars: Solved

---
+++ codewars/lua/grasshopper_debug_sayhello.lua
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/lua
+
+
+local solution = {}
+
+function solution.sayHello(name)
+    return "Hello, " .. name
+end
+
+print(solution.sayHello("bee"))
+
+return solution
