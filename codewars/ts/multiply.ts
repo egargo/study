ffa58762a641746113a6374884be4088e95d91d6
Clint Egargo <clintegargo@protonmail.ch>
2023-06-15 11:11:10 +0800

    Codewars: Solved

---
+++ codewars/ts/multiply.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/typescript
+
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+console.log(multiply(2, 6));
