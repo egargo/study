f086e1cfa1fe36e4d8f08484b6dcc8db1ba72a22
Clint Egargo <clintegargo@protonmail.ch>
2023-06-15 11:06:49 +0800

    Codewars: Solved

---
+++ codewars/go/multiply.go
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/go
+
+
+package main
+
+import "fmt"
+
+func Multiply(a, b int) int {
+    return a * b
+}
+
+func main() {
+    fmt.Println(Multiply(2, 6))
+}
