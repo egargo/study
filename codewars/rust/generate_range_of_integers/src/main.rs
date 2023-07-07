8ae4b9736bdb0b8561b2de03bc9592fcfa218987
Clint Egargo <clintegargo@protonmail.ch>
2023-07-07 14:42:07 +0800

    Codewars: Solved

---
+++ codewars/rust/generate_range_of_integers/src/main.rs
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/55eca815d0d20962e1000106/train/rust
+
+
+fn generate_range(min: usize, max: usize, step: usize) -> Vec<usize> {
+    (min..=max).map(|i| i).step_by(step).collect::<Vec<usize>>()
+}
+
+fn main() {
+    assert_eq!(generate_range(2, 10, 2), [2,4,6,8,10]);
+    assert_eq!(generate_range(1, 10, 3), [1,4,7,10]);
+    assert_eq!(generate_range(1, 10, 1), [1,2,3,4,5,6,7,8,9,10]);
+    assert_eq!(generate_range(1, 10, 4), [1,5,9]);
+    assert_eq!(generate_range(1, 10, 5), [1,6]);
+}
