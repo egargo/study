81dd8bde8dc36d6204f31ca9d3df95ee5df91680
Clint Egargo <clintegargo@protonmail.ch>
2023-07-12 16:33:39 +0800

    Design Patterns (Rust): Builder

---
+++ design-patterns/rust/creational/builder/src/main.rs
@@ -0,0 +1,57 @@
+#[derive(Debug)]
+struct Linux {
+    name: String,
+    base: Option<String>,
+    desktop: Option<String>,
+    active: bool,
+}
+
+#[derive(Debug, Clone)]
+struct LinuxBuilder {
+    name: String,
+    base: Option<String>,
+    desktop: Option<String>,
+    active: bool,
+}
+
+impl Linux {
+    fn new(name: String) -> LinuxBuilder {
+        LinuxBuilder {
+            name,
+            base: None,
+            desktop: None,
+            active: true,
+        }
+    }
+}
+
+impl LinuxBuilder {
+    fn base(&mut self, base: String) -> &mut Self {
+        self.base = Some(base);
+        self
+    }
+
+    fn desktop(&mut self, desktop: String) -> &mut Self {
+        self.desktop = Some(desktop);
+        self
+    }
+
+    fn build(&mut self) -> Linux {
+        Linux {
+            name: self.name.to_owned(),
+            base: self.base.to_owned(),
+            desktop: self.desktop.to_owned(),
+            active: self.active.to_owned(),
+        }
+    }
+}
+
+fn main() {
+    let pop = Linux::new("Pop!_OS".to_owned())
+        .base("Ubuntu".to_owned())
+        .desktop("GNOME".to_string())
+        .build();
+
+    println!("{:?}", pop);
+
+}
