3a28197ef6fe6ad4943717c468a39b06785206be
Clint Egargo <67180945+egargo@users.noreply.github.com>
2023-07-31 11:41:50 +0800

    Codewars: Solved

--- codewars/python/simple_pig_latin.py
+++ codewars/python/simple_pig_latin.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
+
 def pig_it(text):
-	pl = ''
-	for i in text.split(' '):
-		if i not in "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~":
-			first = i[0]
-			i = i.replace(i[0], '', 1)
-			pl += f'{i}{first}ay'
-		else:
-			pl += i
-		pl += ' '
+    """
+    pl = ''
+    for i in text.split(' '):
+        if i not in "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~":
+            first = i[0]
+            i = i.replace(i[0], '', 1)
+            pl += f'{i}{first}ay'
+        else:
+            pl += i
+        pl += ' '
 
-	return pl.strip()
+    return pl.strip()
+    """
 
-if __name__ == '__main__':
-	print(pig_it('Pig latin is cool'))
-	print(pig_it('Hello world !'))
+    return " ".join(
+        [
+            f"{i.replace(i[0], '', 1)}{i[0]}ay"
+            if i not in "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+            else i
+            for i in text.split(" ")
+        ]
+    )
+
+
+if __name__ == "__main__":
+    print(pig_it("Pig latin is cool"))
+    print(pig_it("Hello world !"))
