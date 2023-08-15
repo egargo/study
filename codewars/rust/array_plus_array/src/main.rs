7b30df3e03109a8c3e8786b302c943d34a2f8a0d
Clint Egargo <clintegargo@protonmail.ch>
2023-08-15 15:22:11 +0800

    Codewars: rustfmt

--- codewars/rust/array_plus_array/src/main.rs
+++ codewars/rust/array_plus_array/src/main.rs
@@ -1,6 +1,5 @@
 // https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/rust
 
-
 fn slice_plus_slice(xs: &[i32], ys: &[i32]) -> i32 {
     xs.iter().sum::<i32>() + ys.iter().sum::<i32>()
 }
