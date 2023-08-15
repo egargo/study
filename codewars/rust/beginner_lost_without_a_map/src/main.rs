7ed853c302c757c09bebedda80e69ad12858468a
Clint Egargo <clintegargo@protonmail.ch>
2023-08-15 15:22:36 +0800

    Codewars: rustfmt

--- codewars/rust/beginner_lost_without_a_map/src/main.rs
+++ codewars/rust/beginner_lost_without_a_map/src/main.rs
@@ -1,16 +1,15 @@
 fn maps(values: &Vec<i32>) -> Vec<i32> {
-	let mut doubled = Vec::new();
+    let mut doubled = Vec::new();
 
-	for i in values {
-		doubled.push(i * 2);
-	}
+    for i in values {
+        doubled.push(i * 2);
+    }
 
-	doubled
+    doubled
 }
 
-
 fn main() {
-	let values = vec![1, 2, 3];
+    let values = vec![1, 2, 3];
 
-	println!("{:?}", maps(&values));
+    println!("{:?}", maps(&values));
 }
