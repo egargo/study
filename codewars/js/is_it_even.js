25f4e376249b299a7c9018ca533e37cdc4901319
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-02-04 17:49:13 +0800

    solved: codewars

---
+++ codewars/js/is_it_even.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript
+
+function testEven(n) {
+    if (n % 2 == 0) {
+        return true;
+    }
+
+    return false;
+}
+
+console.log(testEven(12));
+console.log(testEven(0.5));
