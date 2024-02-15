54db7e649cebc1c597dd797499dc85ec631a33a3
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-02-15 14:33:30 +0800

    solved: leetcode

--- leetcode/rust/p283/src/main.rs
+++ leetcode/rust/p283/src/main.rs
@@ -2,7 +2,8 @@ struct Solution;
 
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
-        let zeroes = nums.iter().filter(|i| **i == 0).collect::<Vec<_>>().len();
+        // let zeroes = nums.iter().filter(|i| **i == 0).collect::<Vec<_>>().len();
+        let zeroes = nums.iter().filter(|i| **i == 0).count();
         nums.retain_mut(|i| if *i != 0 { true } else { false });
         nums.append(&mut vec![0; zeroes]);
         println!("{:?}", nums);
