0ee599a4ca1b33a7cee5485fdbedaec0a4c5e796
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-04-09 14:15:14 +0800

    solved: codewars

---
+++ codewars/ts/convert_a_number_to_a_string.ts
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/typescript
+
+export function numberToString(num: number): string {
+    return num.toString();
+}
+
+console.log(numberToString(12));
