f03b64ebd5af7f71da8c284cb804e00c118259f6
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-06-16 14:53:32 +0800

    solved: codewars

---
+++ codewars/python/get_character_from_ascii_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/python
+
+def get_char(c):
+    return chr(c)
+
+if __name__ == "__main__":
+    print(get_char(65))
+    print(get_char(33))
+    print(get_char(34))
+    print(get_char(35))
+    print(get_char(36))
+    print(get_char(37))
+    print(get_char(38))
