4fae2a5ddf5c3e7823f64dce8628e75dc4feffe0
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-06-16 15:06:33 +0800

    solved: codewars

---
+++ codewars/kotlin/multiply.kt
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/kotlin
+
+fun multiply(x: Double, y: Double): Double {
+    return x * y;
+}
+
+fun main() {
+    print(multiply(1.0, 1.0));
+}
